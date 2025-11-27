104dd387161d7d4c5b745b9b95ee8fca1f5382b9
yoovr <yoyovr44@gmail.com>
2025-11-26 21:08:46 -0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,9 +1,61 @@
-from flask import Flask
+from flask import Flask, request, jsonify, Response, redirect
+from datetime import datetime
+import requests
+import json
 
 app = Flask(__name__)
 
-@app.route("/hello")
-def home():
-    return "hey skida"
+@app.route("/api/PlayFabAuthentication", methods=[ "POST"])
+def proxy(path):
+    print(request.get_json())
 
-app.run()
+    headers_to_forward = {}
+    for header in ["X-EntityToken", "X-SecretKey", "X-Authorization", "Content-Type"]:
+        if header in request.headers:
+            headers_to_forward[header] = request.headers[header]
+
+    rjson = request.get_json()
+    
+    print(f"Request body before: {rjson}")
+    if rjson.get("AppId") == "4EF04": # PLAYFAB TITLE ID
+        rjson["OculusId"] = "26124250530509149"
+        rjson["CustomId"] = "OCULUS32786419594305874"
+        rjson["Nonce"]= "0wFQJDq0e0hYkb8bEq8Oq8TCIgWZXKXls4jg4RUzE3bsSCjSiiYh1vnyvwO0c4Ot"
+        urls = "https://sharktagapplab.vercel.app/api/PlayFabAuthentication"
+    elif rjson.get("AppId") == "4EF04": # PLAYFAB TITLE ID
+        rjson["OculusId"] = "26124250530509149"
+        rjson["CustomId"] = "OCULUS32786419594305874"
+        rjson["Nonce"]= "0wFQJDq0e0hYkb8bEq8Oq8TCIgWZXKXls4jg4RUzE3bsSCjSiiYh1vnyvwO0c4Ot"
+        urls = "https://sharktagapplab.vercel.app/api/PlayFabAuthentication"
+    elif rjson.get("AppId") == "4EF04": # PLAYFAB TITLE ID
+        rjson["OculusId"] = "26124250530509149"
+        rjson["CustomId"] = "OCULUS32786419594305874"
+        rjson["Nonce"]= "0wFQJDq0e0hYkb8bEq8Oq8TCIgWZXKXls4jg4RUzE3bsSCjSiiYh1vnyvwO0c4Ot"
+        urls = "https://sharktagapplab.vercel.app/api/PlayFabAuthentication"
+    elif rjson.get("AppId") == "4EF04": # PLAYFAB TITLE ID
+        urls = "https://sharktagapplab.vercel.app/api/PlayFabAuthentication"
+    
+    req = requests.request(
+        method=request.method,
+        url=urls,
+        headers=headers_to_forward,
+        json=rjson,
+        params=request.args,
+        allow_redirects=False
+    )
+
+    print(f"Request body after: {request.get_data()}")
+
+    if req.status_code in [301, 302]:
+        return redirect(req.headers["Location"], code=req.status_code)
+
+    try:
+        response_data = req.json()
+        
+        print("JSON Response:", response_data)
+        return jsonify(response_data), req.status_code
+    except ValueError:
+        return Response(req.content, status=req.status_code, headers=dict(req.headers))
+        
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5008)
